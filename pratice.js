1894ea70bacd9c7ac043a4449d2c358ea3a24840
NightSea <sweetmilkshake0324@gmail.com>
2022-07-11 15:46:34 +0800

    feat: 新增 練習小程式

---
+++ pratice.js
@@ -0,0 +1,55 @@
+// 使用for & while 印出1到9
+console.log("print 1 to 9");
+console.log("use for");
+
+let input = 9;
+
+// for
+for (var i = 0 ; i < input ; i++){
+    console.log(i + 1);
+}
+
+// while
+console.log("use while");
+var i = 0;
+while (i < input){
+    console.log(i + 1);
+    i++;
+}
+
+// print函式，印出1到n
+console.log("print");
+console.log(print(9));
+function print(n){
+    let result = "";
+
+    for (var i = 0 ; i < n ; i++){
+        result += (i + 1) + "\n";
+    }
+    return result;
+}
+
+// star函式，印出n個*
+console.log("star");
+console.log(star(9));
+function star(n){
+    let result = "";
+
+    for (var i = 0 ; i < n ; i++){
+        result += "*";
+    }
+    return result;
+}
+
+// 測第一字母是否為大寫
+console.log("charAtIsUpperCase")
+console.log(isUpperCase("Avb"))
+function isUpperCase(str){
+    let firstChar = str.charAt();
+    
+    if (firstChar == firstChar.toUpperCase()){
+        return true
+    }else{
+        return false
+    }
+}
